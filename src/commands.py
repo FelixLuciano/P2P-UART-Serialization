19ac4331870d9edacb0b21e3c16c5b0cc7643df7
FelixLuciano <felix.lucianodias@gmail.com>
2022-03-01 16:34:01 -0300

    Reduzida responsabilidade da biblioteca de comandos

--- src/commands.py
+++ src/commands.py
@@ -1,3 +1,4 @@
+import binascii
 import random
 
 
@@ -6,7 +7,7 @@ class Commands:
 
     @classmethod
     def get (self, *values):
-        return bytearray(values)
+        return bytes(bytearray(values))
 
 
     @classmethod
@@ -24,28 +25,9 @@ class Commands:
         return random.choices(self.commands, k=size)
 
 
-    @classmethod
-    def wrap (self, content, prefix=0b01010000, sufix=0b00000101):
-        return self.get(prefix) + content + self.get(sufix)
-
-
-    @classmethod
-    def getStream (self, content, separator=0b01010101, *args):
-        return self.wrap(self.get(separator).join(content), *args)
-
-    
-    @classmethod
-    def getSequenceStream (self, size, *args):
-        return self.getStream(self.getSequence(size), *args)
-
-
-Commands.add(0x00, 0xFF, 0x00, 0xFF)
-Commands.add(0x00, 0xFF, 0xFF, 00)
-Commands.add(0xFF)
-Commands.add(0x00)
-Commands.add(0xFF, 0x00)
-Commands.add(0x00, 0xFF)
-
-# print(Commands.getStream(Commands.getSequence(random.randint(10, 30))))
-
-print(Commands.getSequence(10))
+# Commands.add(0x00, 0xFF, 0x00, 0xFF)
+# Commands.add(0x00, 0xFF, 0xFF, 00)
+# Commands.add(0xFF)
+# Commands.add(0x00)
+# Commands.add(0xFF, 0x00)
+# Commands.add(0x00, 0xFF)
