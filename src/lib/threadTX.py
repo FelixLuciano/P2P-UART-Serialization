75a068e14f24afc156bb387b7b76145018727438
FelixLuciano <felix.lucianodias@gmail.com>
2022-03-17 12:37:20 -0300

    Estrutura semelhante ao RX

--- src/lib/threadTX.py
+++ src/lib/threadTX.py
@@ -5,21 +5,32 @@ from lib.interface import Interface
 
 
 class TX:
+    TRANSMIT_PREIOD = 0.01
+
+
     def __init__ (self, interface:Interface):
         self.interface = interface
         self.buffer = []
         self.threadStop = False
+        self.threadPaused = True
 
 
     def _thread (self):
         while not self.threadStop:
-            if len(self.buffer) > 0:
-                self.interface.write(self.buffer[0])
-                self.buffer.pop(0)
+            if not self.threadPaused:
+                if not self.isEmpty():
+                    data = self.buffer[0]
+
+                    self.interface.write(data)
+                    self.buffer.pop(0)
+                else:
+                    self.pause()
+
+            time.sleep(self.TRANSMIT_PREIOD)
 
 
     def enable (self):
-        self.thread = Thread(target=self._thread, args=())
+        self.thread = Thread(target=self._thread)
         self.threadStop = False
 
         self.thread.start()
@@ -29,6 +40,29 @@ class TX:
         self.threadStop = True
 
 
+    def pause (self):
+        self.threadPaused = True
+
+
+    def resume (self):
+        self.threadPaused = False
+
+
+    def getLen (self):
+        return len(self.buffer)
+
+
+    def isEmpty (self):
+        return self.getLen() == 0
+
+
+    def clear (self):
+        self.buffer *= 0
+
+
     def transmit (self, data:bytes):
         self.buffer.append(data)
-        time.sleep(.05)
+        self.resume()
+
+        while not self.isEmpty():
+            time.sleep(self.TRANSMIT_PREIOD)
