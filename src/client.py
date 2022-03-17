21c08c45664e94d1f8950739b81285cb5b90ce59
FelixLuciano <felix.lucianodias@gmail.com>
2022-03-17 12:42:50 -0300

    Cliente envia imagem

--- src/client.py
+++ src/client.py
@@ -1,7 +1,7 @@
-import time
+# import time
 
 from lib.thread import Thread
-from lib.commands import Commands
+from lib.commands import tryServerConnection
 from lib.stream import Stream
 
 
@@ -9,17 +9,22 @@ from lib.stream import Stream
 # SERIAL_NAME = '/dev/tty.usbmodem1411' # Mac
 SERIAL_NAME = 'COM3'                  # Windows
 
+INPUT_IMAGE  = 'image.webp'
+
 
 def main ():
     com1 = Thread(SERIAL_NAME)
 
     try:
         com1.enable()
-        Commands.tryServerConnection(com1)
+        tryServerConnection(com1, timeout=5)     
 
-        stream = Stream('response', b'Hello World')
-        stream.submit(com1)
+        with open(INPUT_IMAGE, 'rb') as file:
+            package = Stream(data=file.read())
 
+        package.submit(com1)
+    except KeyboardInterrupt:
+        pass
     except Exception as error:
         print('ops! :-\\\n', error)
 
