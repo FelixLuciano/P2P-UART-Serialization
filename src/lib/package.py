ee240c219f878c94a8ff00b58f56cc41091f74c8
FelixLuciano <felix.lucianodias@gmail.com>
2022-04-12 18:57:30 -0300

    Timeout de requisições, e sucesso apenas em dados

--- src/lib/package.py
+++ src/lib/package.py
@@ -8,7 +8,7 @@ class Package:
     EOP = b'\xAA\xBB\xCC\xDD'
 
 
-    def __init__ (self, type_:str='data', length:int=1, index:int=1, data:bytes=b''):
+    def __init__ (self, type_:str='data', length:int=None, index:int=None, data:bytes=b''):
         self.type = type_
         self.length = length
         self.index = index
@@ -32,80 +32,82 @@ class Package:
 
 
     @classmethod
-    def request (cls, thread:Thread, type_:str=None, index:int=None, length:int=None, size:int=None, timeout:int=None):
-        header = Header.request(thread, timeout=timeout/3)
-        data, _ = thread.receive(size=header.size, timeout=timeout/3)
-        eop, _ = thread.receive(size=len(cls.EOP), timeout=timeout/3)
+    def request (cls, thread:Thread, type_:str=None, index:int=None, length:int=None, size:int=None, timeout:int=20):
+        header = Header.request(thread=thread, type_=type_, length=length, index=index, size=size, timeout=timeout/3)
+        data, len_data = thread.receive(size=header.size, timeout=timeout/3)
+        eop, len_eop = thread.receive(size=len(cls.EOP), timeout=timeout/3)
 
-        if (type_ and header.type != type_ or
+        if header.type == 'timeout' or len_data == 0 or len_eop == 0:
+            package = TimeOut()
+
+            package.submit(read=thread, timeout=timeout/3)
+
+            return package
+
+        elif (type_ and header.type != type_ or
             index and header.index != index or
             length and header.length != length or
-            size and header.size != size or
+            size and header.size != size and len_data == size or
             eop != cls.EOP
         ):
-            return Error(index=index)
+            error = Error(index=index)
 
-        if header.type != 'success':
+            error.submit(thread=thread, timeout=timeout)
+
+            return error
+
+        elif header.type == 'data':
             Success(index=index).submit(thread=thread, timeout=timeout)
 
-        types = {
-            'request': Request,
-            'response': Response,
-            'data': Data,
-            'success': Success,
-            'timeout': TimeOut,
-            'error': Error
-        }
-
-        return types.get(header.type, Data)(
+        return cls(
             type_=header.type,
             index=header.index,
             length=header.length,
-            data=data,
-            id = header.id
+            data=data
         )
 
 
     def submit (self, thread:Thread, timeout:int=-1):
+        submit_success = True
+
         while True:
             thread.transmit(self.encode())
-            if self.type != 'success':
+
+            if self.type == 'data':
                 success, done = self.getSuccessDone(
                     thread=thread,
                     index=self.index,
-                    message=f'Failed do submit {self.type} at {self.index} of {self.length}.',
                     timeout=timeout
                 )
 
-                if success and done:
+                if done:
+                    submit_success = success
                     break
-                if not success and done:
-                    exit()
             else:
                 break
 
+        return submit_success
+
 
     @classmethod
-    def getSuccessDone (cls, thread:Thread, index:int=1, message:str='Attempt failed.', timeout:int=None):
+    def getSuccessDone (cls, thread:Thread, index:int=1, timeout:int=None):
         response = cls.request(thread, type_='success', index=index, timeout=timeout)
+        success = False
+        done = False
 
-        if  response.type == 'error':
-            print('[Error]', message, end=' ')
-
-            tryAgain = input('Try again? y/n ')
-
-            if tryAgain.lower() not in ('y', 'yes'):
-                return False, True
-            else:
-                return False, False
+        if response.type == 'error':
+            done = True
         elif response.type == 'success':
-            return True, True
+            success = True
+            done = True
         else:
-            return False, True
+            done = True
+
+        return success, done
 
 
 class Request (Package):
-    def __init__ (self, target:int=None, length:int=0):
+    def __init__ (self, target:int=0, length:int=None):
         super().__init__(type_='request', length=length, index=target)
         self.target = target
 
@@ -121,10 +123,8 @@ class Data (Package):
 
 
 class Success (Package):
-    def __init__ (self, package_id:int=None):
-        super().__init__(type_='success', index=package_id)
-        
-        self.package_id = package_id
+    def __init__ (self, index:int=None):
+        super().__init__(type_='success', index=index)
 
 
 class TimeOut (Package):
@@ -133,7 +133,5 @@ class TimeOut (Package):
 
 
 class Error (Package):
-    def __init__ (self, package_id:int=None):
-        super().__init__(type_='error', index=package_id)
-        
-        self.package_id = package_id
+    def __init__ (self, index:int=None):
+        super().__init__(type_='error', index=index)
