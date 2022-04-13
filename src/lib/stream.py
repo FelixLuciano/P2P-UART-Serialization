555ee6d0e259655d6e4c6397f30d87780920a347
FelixLuciano <felix.lucianodias@gmail.com>
2022-04-13 18:01:52 -0300

    Data streaming

--- src/lib/stream.py
+++ src/lib/stream.py
@@ -4,10 +4,10 @@ from alive_progress import alive_bar
 
 from lib.thread import Thread
 from lib.header import Header
-from lib.package import Package, Request, Data
+from lib.package import Package, Request, Response
 
 
-class Stream:
+class Data_stream:
     PACKAGE_SIZE = 128
     PAYLOAD_SIZE = PACKAGE_SIZE - Header.SIZE - len(Package.EOP)
 
@@ -21,10 +21,12 @@ class Stream:
 
 
     def encode(self):
+        length=len(self)
+
         for index, step in enumerate(range(0, len(self.data), self.PAYLOAD_SIZE)):
             payload = self.data[step:step+self.PAYLOAD_SIZE]
 
-            yield Data(length=len(self), index=index, data=payload)
+            yield Data(length=length, index=index, data=payload)
 
 
     @classmethod
@@ -35,48 +37,43 @@ class Stream:
 
 
     @classmethod
-    def request (cls, thread:Thread, target:int=0, timeout:int=-1):
+    def request (cls, thread:Thread, target:int=0, timeout:int=None):
         buffer = []
         count = 1
 
         request = Request(target=target).request(thread=thread, timeout=timeout)
 
+        Response().submit(thread=thread, timeout=timeout)
+
         with alive_bar(title='Receiving', manual=True) as bar:
             while True:
-                response = Package.request(thread, type_='data', index=count, timeout=timeout)
+                response = Data(length=request.length, index=count).request(thread=thread, timeout=timeout)
 
-                if response.type == 'data':
-                    buffer.append(response)
-                    bar(count / request.length)
-
-                    if count < request.length:
-                        count == 1
-                    else:
-                        break
-                elif response.type == 'timeout':
+                if response.type == 'timeout':
                     return None
+                
+                buffer.append(response.data)
+
+                if response.index < request.length:
+                    bar(count / request.length)
+                    count += 1
                 else:
-                    thread.rx.clear()
+                    break
 
         return cls.decode(buffer)
 
 
-    def submit (self, thread:Thread, target:int=0, timeout:int=-1):
-        # while True:
-        #     with alive_bar(len(self), title='Sending') as bar:
-        #         for package in self.encode():
-        #             package.submit(thread, timeout=timeout)
-        #             bar()
+    def submit (self, thread:Thread, target:int=0, timeout:int=None):
+        Request(target=target).submit(thread=thread, timeout=timeout)
 
-        #     success, done = Package.getSuccessDone(
-        #         thread=thread,
-        #         message=f'Failed do submit {self.type} stream.',
-        #         timeout=timeout
-        #     )
+        response = Response().request(thread=thread, timeout=timeout)
 
-        #     if success and done:
-        #         break
-        #     else:
-        #         thread.tx.clear()
-        #         continue
-        return None
+        if response.type == 'timeout':
+            return False
+        
+        with alive_bar(title='Submiting', manual=True) as bar:
+            for count, package in enumerate(self.encode()):
+                package.submit(thread=thread, timeout=timeout)
+                bar(count / len(self))
+
+        return True
